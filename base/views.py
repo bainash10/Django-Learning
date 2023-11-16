036b2bee52049c3de19eea65d488c86fdab57f1d
Bainash <nischalbaidar@yahoo.com>
2023-11-16 14:30:25 +0545

    Day 04

--- base/views.py
+++ base/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
-rooms=[
-    {'id':1, 'name':'Lets Learn Python!'},
-    {'id':2, 'name':'Design With me'},
-    {'id':3, 'name':'Frontend Developers'},
-]
+# rooms=[
+#     {'id':1, 'name':'Lets Learn Python!'},
+#     {'id':2, 'name':'Design With me'},
+#     {'id':3, 'name':'Frontend Developers'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context={'rooms':rooms}
     return render(request, 'base/home.html', {'rooms':rooms})
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id']==int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+    # room = None
+    # for i in rooms:
+    #     if i['id']==int(pk):
+    #         room = i
     context = {'room':room}
     return render(request, 'base/room.html', context)
