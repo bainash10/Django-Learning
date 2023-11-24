db15b1888ee3acc97a90f67714936295c787921f
Bainash <nischalbaidar@yahoo.com>
2023-11-24 21:05:18 +0545

    Day 7

--- base/views.py
+++ base/views.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 # rooms=[
@@ -16,11 +17,12 @@ from django.contrib.auth import authenticate, login, logout
 # ]
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method=='POST':
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
 
         try:
@@ -36,12 +38,29 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or password doesnot exist')
 
-    context={}
+    context={'page':page}
     return render(request,'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    # page='register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form= UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username =user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An Error occured during registration')
+    
+    return render(request, 'base/login_register.html', {'form': form})
 
 
 def home(request):
