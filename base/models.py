5fe5dcd6006d029b76a0d7cc536d4c0b9b81d2a2
Bainash <nischalbaidar@yahoo.com>
2024-03-05 20:22:00 +0545

    Day 15

--- base/models.py
+++ base/models.py
@@ -1,34 +1,49 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
 class Topic(models.Model):
-    name= models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
 
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True) #blank means can be left blanks
-    participants = models.ManyToManyField(User, related_name='participants', blank=True)
-    updated=models.DateTimeField(auto_now=True) #auto_now takes snapshot everytime
-    created = models.DateTimeField(auto_now_add=True) #auto_now_add takes snapshot when we save the instance, for initial time
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-updated', '-created']
 
-    def  __str__(self):
+    def __str__(self):
         return self.name
+
 
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE) #cascade is used to delete all the messages if rooms get deleted
-    body=models.TextField()
-    updated=models.DateTimeField(auto_now=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ['-updated', '-created']
 
